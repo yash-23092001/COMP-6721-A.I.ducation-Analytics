2a2b675e2318379101cbe26a02143d40a004d3b8
yash-23092001 <yash.chhelaiya@gmail.com>
2023-10-26 14:21:38 -0400

    Split the data into train and test

---
+++ DataPartition.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+import random
+
+labels = ["bored", "confused", "distracted", "focused", "neutral"]
+
+def split(nameIn, source_dir):
+    train_dir = "./Student-engagement-dataset/train/" + nameIn
+    test_dir = "./Student-engagement-dataset/test/" + nameIn
+
+    # Create destination directories if they don't exist
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
+
+    # List all files in the source directory
+    file_list = os.listdir(source_dir)
+
+    # Shuffle the file list randomly
+    random.shuffle(file_list)
+
+    # Calculate the split point based on 80% for training and 20% for testing
+    split_point = int(0.8 * len(file_list))
+
+    # Copy files to train directory
+    for filename in file_list[:split_point]:
+        source_path = os.path.join(source_dir, filename)
+        destination_path = os.path.join(train_dir, filename)
+        shutil.copy(source_path, destination_path)
+
+    # Copy files to test directory
+    for filename in file_list[split_point:]:
+        source_path = os.path.join(source_dir, filename)
+        destination_path = os.path.join(test_dir, filename)
+        shutil.copy(source_path, destination_path)
+
+
+for name in labels:
+    # Define the source and destination directories
+    dirS = "./Dataset/" + name
+    split(name, dirS)
